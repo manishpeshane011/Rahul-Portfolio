2add654898bf528038f28c67078e8b59b6d44f8e
Manish-202515 <manishp@thrikaalverse.com>
2026-04-19 21:41:25 +0530

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/Rahul-Portfolio/',   // 🔥 THIS IS THE FIX
+  base: '/Rahul-Portfolio/',  // 🔥 MUST MATCH repo name exactly
 })
